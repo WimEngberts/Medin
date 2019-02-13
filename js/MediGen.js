f1e804227eba5d6df83dbb72be16bb4628e2660a
WimEngberts <wim.engberts@centric.eu>
2019-02-13 13:22:40 +0100

    Add files via upload

--- js/MediGen.js
+++ js/MediGen.js
@@ -34,7 +34,7 @@ function onDeviceReady()
 		alert ('no database available!');
 	setFontSizes ();
 	getPincode ();
-	setNextNotification ();
+//	setNextNotification ();
 }
 
 function addEnterListener (listenFunction)
